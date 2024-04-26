67522fe6b777c4b7abe6f57e69b4d62c40a90c23
Josue Borden <josue.borden6@gmail.com>
2024-04-26 15:34:14 -0600

    Segundo Commit

--- src/lab2p1_josueborden/Lab2P1_JosueBorden.java
+++ src/lab2p1_josueborden/Lab2P1_JosueBorden.java
@@ -29,19 +29,47 @@ public class Lab2P1_JosueBorden {
                 System.out.println("Ingrese el numero:");
                 num = lea.nextInt();
                 int decision = 0;
+                int residuo = 0;
+                int sumaimpar = 0;
+                int sumapar = 0;
+                int residuo2 = 0;
                 System.out.println("Desea sumar los digitos pares (1) o impares(2):");
                 decision = lea.nextInt();
-                if(decision == 1){
-                    
+                while(num > 0){
+                    residuo = num % 10;
+                    num = num / 10;
+                    residuo2 = residuo % 2;
+                    if(residuo2 == 0){
+                        sumapar = sumapar + residuo;
+                    }
+                    else{
+                        sumaimpar = sumaimpar + residuo;
+                    }
                 }
-                    
-                
+                if(decision == 1){
+                    System.out.println("La suma de los numeros pares es: " + sumapar);
+                }
+                else{
+                    System.out.println("La suma de los numeros impares es:" + sumaimpar);
+                }
+                  
+            } 
+            
+            else if(ejercicio == 2){
+                int num = 0;
+                System.out.println("Ingrese un numero:");
+                num = lea.nextInt();
                 
             }
-                
             
-            
+            else if(ejercicio == 3){
+                int num = 0;
+                while(num < 999999999){
+                    System.out.println("Ingrese un numero de 10 caracteres:");
+                    num = lea.nextInt();
+                } 
+                System.out.println("num es valido");
+            }
         }
     }
-    
 }
